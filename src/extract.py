e4f4777016559a6b1de9f81ba43076fed42d02b0
ana.medina <ana.medina@segurosbolivar.com>
2026-03-15 18:58:36 -0500

    Cambios para entorno de pruebas

--- src/extract.py
+++ src/extract.py
@@ -12,7 +12,7 @@ def extract_siniestros(limit: int = 100):
     
     try:
         # 2. Definimos y ejecutamos la consulta
-        query = text('SELECT * FROM "ecosistema_clientes"."t_siniestros" LIMIT :limit')
+        query = text('SELECT "NUMERO_POLIZA","NOMBRE_RAMO_EMISION","NOMBRE_PRODUCTO","NUMERO_SINIESTRO","FECHA_SINIESTRO","FECHA_AVISO","DESCRIPCION_CAUSA","DESCRIPCION_SINIESTRO","MUNICIPIO_SINIESTRO","DEPARTAMENTO_SINIESTRO" FROM "ecosistema_clientes"."t_siniestros" LIMIT :limit')
         result = db.execute(query, {"limit": limit})
         
         # 3. Transformamos el resultado a una lista de diccionarios
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,14 +1,21 @@
 from extract import extract_siniestros
+from transform import transform_data_siniestros
 
 
 def main():
     """
-    Extrae siniestros desde Postgres (stage).
+    Extrae siniestros desde Postgres (stage) y los guarda en data/processed como CSV.
     """
-    print("Extrayendo siniestros...")   
+    print("Iniciando pipeline de extracción de siniestros...")
     siniestros = extract_siniestros()
-    print(f"Filas obtenidas: {len(siniestros)}")
+    print("Extracción de siniestros completada.")
 
+    print("Iniciando transformación de siniestros...    ")
+    siniestros_csv = transform_data_siniestros(siniestros)
+    print(f"Siniestros transformados y guardados en: {siniestros_csv}")
+    print("Transformación de siniestros completada.")
+
+    print("Pipeline de extracción de siniestros completada.")
 
 if __name__ == "__main__":
     main()
